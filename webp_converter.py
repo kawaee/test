69cd18a74b2274c9850735320df7fc201d87a8b7
kawaee <kawaee@proton.me>
2023-10-01 19:10:58 +0900

    second commit

--- webp_converter.py
+++ webp_converter.py
@@ -5,17 +5,16 @@ def convert_images_to_webp(input_folder):
     image_files = [f for f in os.listdir(input_folder) if f.endswith(('.png', '.jpg', '.jpeg'))]
 
     for image_file in image_files:
-        # 이미지 파일 경로
+        # image path
         input_image_path = os.path.join(input_folder, image_file)
 
-        # WebP로 변환된 이미지 파일 경로 (같은 폴더에 저장)
+        # output path to WebP (same diraction)
         output_image_path = os.path.splitext(input_image_path)[0] + ".webp"
 
-        # 이미지를 WebP로 변환
+        # convert to WebP
         img = Image.open(input_image_path)
         img.save(output_image_path, "webp")
 
 if __name__ == "__main__":
-    input_folder = input("이미지 파일들이 있는 폴더 경로를 입력하세요: ")
-
+    input_folder = input("please input your images path: ")
     convert_images_to_webp(input_folder)
